9ac1df76edb8b1502210c31a09fa2c68ba2c3a3d
XOL10 <69771141+XOL10@users.noreply.github.com>
2021-02-28 22:10:35 -0600

    Delete SomethingIsWrong directory

--- SomethingIsWrong/src/main/java/SomethingIsWrong.java
+++
@@ -1,8 +0,0 @@
-public class SomethingIsWrong {
-    public static void main(String[] args) {
-        Rectangle myRect = new Rectangle();//el método Rectangle no estaba creado
-        myRect.width = 40;
-        myRect.height = 50;
-        System.out.println("myRect's area is " + myRect.getArea());//Se creó el metodo getArea
-    }
-}
